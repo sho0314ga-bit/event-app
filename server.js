0587a3c60885610b4c545dedbea7335bd4ebdcae
sho0314ga-bit <sho0314ga@gmail.com>
2026-09-18 14:06:44 +0900

    イベント追加POST APIを実装

--- server.js
+++ server.js
@@ -1,9 +1,29 @@
 const express = require("express");
 const app = express();
+app.use(express.json());
+
+
+const events = [
+  {
+    name: "ライブA",
+    venue: "東京ドーム"
+  },
+  {
+    name: "ライブB",
+    venue: "日本武道館"
+  }
+];
 
 app.get("/events",(req,res)=>{
-res.json(["ライブA","ライブB"]);
+res.json(events);
 });
+
+app.post("/events",(req,res)=>{
+    const newEvent = req.body;
+    events.push(newEvent);
+    res.json(events);
+}
+);
 
 app.listen(3000,() => {
     console.log("サーバーが起動しました");
