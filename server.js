efef4fccccacb35eeb4fb2df3340e826e6f0ab9b
sho0314ga-bit <sho0314ga@gmail.com>
2026-09-20 18:41:05 +0900

    SQLiteでイベントデータを永続化

--- server.js
+++ server.js
@@ -1,29 +1,40 @@
 const express = require("express");
+const Database = require("better-sqlite3");
 const app = express();
+const db = new Database("events.db");
+
+
+db.exec(`
+  CREATE TABLE IF NOT EXISTS events (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    name TEXT NOT NULL,
+    venue TEXT NOT NULL
+  )
+`);
+
+const insert = db.prepare(`
+  INSERT INTO events (name, venue)
+  VALUES (?, ?)
+`);
+
+const select = db.prepare(`
+  SELECT * FROM events
+`);
+
 app.use(express.json());
 
 
-const events = [
-  {
-    name: "ライブA",
-    venue: "東京ドーム"
-  },
-  {
-    name: "ライブB",
-    venue: "日本武道館"
-  }
-];
-
 app.get("/events",(req,res)=>{
-res.json(events);
+ const rows = select.all();
+    res.json(rows);
 });
 
 app.post("/events",(req,res)=>{
     const newEvent = req.body;
-    events.push(newEvent);
-    res.json(events);
-}
-);
+    insert.run(newEvent.name,newEvent.venue);
+    const rows = select.all();
+    res.json(rows);
+});
 
 app.listen(3000,() => {
     console.log("サーバーが起動しました");
