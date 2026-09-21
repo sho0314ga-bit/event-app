401b8fab10e64ee275b264eed38033614604f32e
sho0314ga-bit <sho0314ga@gmail.com>
2026-09-21 12:57:19 +0900

    UPDATE・DELETE APIを実装

--- server.js
+++ server.js
@@ -1,8 +1,11 @@
 const express = require("express");
 const Database = require("better-sqlite3");
+
+
 const app = express();
 const db = new Database("events.db");
 
+app.use(express.json());
 
 db.exec(`
   CREATE TABLE IF NOT EXISTS events (
@@ -21,7 +24,16 @@ const select = db.prepare(`
   SELECT * FROM events
 `);
 
-app.use(express.json());
+const update = db.prepare(`
+  UPDATE events
+  SET name = ? , venue = ?
+  WHERE id = ?
+`);
+
+const deleteEvent = db.prepare(`
+  DELETE FROM events
+  WHERE id= ?
+  `);
 
 
 app.get("/events",(req,res)=>{
@@ -31,9 +43,28 @@ app.get("/events",(req,res)=>{
 
 app.post("/events",(req,res)=>{
     const newEvent = req.body;
+
     insert.run(newEvent.name,newEvent.venue);
+    
     const rows = select.all();
     res.json(rows);
+});
+
+app.patch("/events/:id",(req,res)=>{
+  const newEvent = req.body;
+
+  update.run(newEvent.name,newEvent.venue,req.params.id);
+
+  const rows = select.all();
+  res.json(rows);
+});
+
+app.delete("/events/:id",(req,res)=>{
+  
+  deleteEvent.run(req.params.id);
+
+  const rows = select.all();
+  res.json(rows);
 });
 
 app.listen(3000,() => {
